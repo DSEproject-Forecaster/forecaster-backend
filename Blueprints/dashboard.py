c7507e2f636e0451a748be206fafecff4d146042
KavishkaGunathilaka <gihanthakavishka@gmail.com>
2022-10-10 23:29:12 +0530

    Implemented charts

--- Blueprints/dashboard.py
+++ Blueprints/dashboard.py
@@ -8,61 +8,110 @@ dashboard_bp = Blueprint('dashboard', __name__)
 def getDailySeasonalData():
     daily_df = pd.read_csv('./static/csv/daily_seasonal.csv')
 
-    out = {
-        'labels': daily_df.index.tolist(),
-        'datasets': [],
-        'borderColor': ''
-    }
+    out = []
 
     for col in daily_df.columns:
-        r = randint(0, 255)
-        g = 255 - r
-        b = randint(0, 255)
-        out['datasets'].append({
-            'label': col,
-            'data': daily_df[col].values.tolist(),
-            'borderColor':'rgb(' + str(r) + ',' + str(g) + ',' + str(b) + ')'
-        })
+        trace = {
+            'x': [i for i in range(24)],
+            'y': daily_df[col].values.tolist(),
+            'mode': 'lines',
+            'name': col
+        }
+        out.append(trace)
     return jsonify(out)
 
 @dashboard_bp.route('/getYearlySeasonalData/', methods=['GET'])
 def getYearlySeasonalData():
     yearly_df = pd.read_csv('./static/csv/yearly_seasonal.csv')
 
-    out = {
-        'labels': yearly_df.index.tolist(),
-        'datasets': [],
-        'borderColor': ''
-    }
+    out = []
 
-    for col in yearly_df.columns:
-        r = randint(0, 255)
-        g = 255 - r
-        b = randint(0, 255)
-        out['datasets'].append({
-            'label': col,
-            'data': yearly_df[col].values.tolist(),
-            'borderColor':'rgb(' + str(r) + ',' + str(g) + ',' + str(b) + ')'
-        })
+    colList = list(yearly_df.columns)
+    colList.remove('time_stamp')
+
+    for col in colList:
+        trace = {
+            'x': yearly_df.time_stamp.values.tolist(),
+            'y': yearly_df[col].values.tolist(),
+            'type': "scatter",
+            'mode': "lines",
+            'name': col
+        }
+        out.append(trace)
     return jsonify(out)
 
 @dashboard_bp.route('/getTrends/', methods=['GET'])
 def getTrends():
     trend_df = pd.read_csv('./static/csv/trends.csv')
 
-    out = {
-        'labels': trend_df.index.tolist(),
-        'datasets': [],
-        'borderColor': ''
-    }
+    out = []
 
-    for col in trend_df.columns:
-        r = randint(0, 255)
-        g = 255 - r
-        b = randint(0, 255)
-        out['datasets'].append({
-            'label': col,
-            'data': trend_df[col].values.tolist(),
-            'borderColor':'rgb(' + str(r) + ',' + str(g) + ',' + str(b) + ')'
-        })
+    colList = list(trend_df.columns)
+    colList.remove('time_stamp')
+
+    for col in colList:
+        trace = {
+            'x': trend_df.time_stamp.values.tolist(),
+            'y': trend_df[col].values.tolist(),
+            'type': "scatter",
+            'mode': "lines",
+            'name': col
+        }
+        out.append(trace)
+    return jsonify(out)
+
+@dashboard_bp.route('/getCorrelations/', methods=['GET'])
+def getCorrelations():
+    df = pd.read_csv('./static/csv/weather.csv').drop('time_stamp', axis=1)
+
+    out = [
+        {
+            'z': df.corr().to_numpy().tolist(),
+            'x': list(df.columns),
+            'y': list(df.columns),
+            'type': 'heatmap'
+        }
+    ]
+
+    return jsonify(out)
+
+@dashboard_bp.route('/getBoxPlots/', methods=['GET'])
+def getBoxPlots():
+    df = pd.read_csv('./static/csv/weather.csv').drop('time_stamp', axis=1)
+
+    out = []
+
+    for col in df.columns:
+        trace = {
+            'y': df[col].values.tolist(),
+            'type': 'box',
+            'name': col
+        }
+        out.append(trace)
+
+    return jsonify(out)
+
+@dashboard_bp.route('/getRadar/', methods=['GET'])
+def getRadar():
+    df = pd.read_csv('./static/csv/weather.csv')
+    df['time_stamp'] = pd.to_datetime(df['time_stamp'])
+
+    df = df[df['time_stamp'].dt.year==2014]
+    # df2 = df.iloc[-1]
+    # df.append(df2)
+    # print(df2.shape, df.shape)
+    out = []
+
+    colList = list(df.columns)
+    colList.remove('time_stamp')
+
+    for col in colList:
+        trace = {
+            'type': 'scatterpolar',
+            'r': df[col].values.tolist(),
+            'theta': ['Jan','Feb','Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan'],
+            'name': col
+        }
+        out.append(trace)
+
     return jsonify(out)
