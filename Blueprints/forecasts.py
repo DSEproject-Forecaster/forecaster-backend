df99fe54e68b1a93cbf00cbffd94356e97282681
KavishkaGunathilaka <gihanthakavishka@gmail.com>
2022-12-29 22:40:43 +0530

    Monthly predictions finalized

--- Blueprints/forecasts.py
+++ Blueprints/forecasts.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, jsonify
 import db.dbHelper as db
 from Utils.timeUtils import getCurrentTime
+from dateutil.relativedelta import relativedelta
 import datetime
+import pandas as pd
 
 forecasts_bp = Blueprint('forecasts', __name__)
 
@@ -21,3 +23,30 @@ def getWeeklyPredictions():
     timestamps = [(base + datetime.timedelta(days=x)).strftime("%Y-%m-%d %H:00:00") for x in range(7)]
     predictions = db.getPrediction(timestamps)
     return jsonify(predictions)
+
+@forecasts_bp.route('/getMonthlyRadar/', methods=['GET'])
+def getMonthlyRadar():
+    curTime = getCurrentTime()
+    base =  datetime.datetime.strptime(curTime, "%Y-%m-%d %H:%M")
+    timestamps = [(base + relativedelta(months=+x)).strftime("%Y-%m-%d %H:00:00") for x in range(12)]
+    predictions = db.getPrediction(timestamps)
+    out=[]
+    attributes = list(predictions[0].keys())
+    attributes.remove('time_stamp')
+
+    theta = []
+    for timestamp in timestamps:
+        theta.append(datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S").strftime('%b %y'))
+
+    for col in attributes:
+        vals=[]
+        for month in predictions:
+            vals.append(month[col])
+        trace = {
+            'type': 'scatterpolar',
+            'r': vals + [vals[0]],
+            'theta': theta + [theta[0]],
+            'name': col
+        }
+        out.append(trace)
+    return jsonify(out)
